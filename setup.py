95d7845f588495b82fba35e3f04be88dfef866a6
illbison <105686563+illbison@users.noreply.github.com>
2024-02-06 20:02:09 +0300

    Update

--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@ import os
 
 from setuptools import setup
 
-requirements = ["olefile", "pyzipper", "yara-python", "pefile", "peutils"]
+requirements = ["olefile", "pyzipper", "yara-python"]
 
 # build console_scripts list automatically
 modules = [module for module in os.listdir("./src") if module.endswith(".py") and module != "__init__.py"]
-console_scripts = [f"{module.replace('_', '-')} = src.{module.replace('.py', '')}:Main" for module in modules]
+console_scripts = [
+    f"{module.replace('_', '-').replace('.py', '')} = src.{module.replace('.py', '')}:Main" for module in modules
+]
 
 setup(
     name="ds-pip",
     description="Make several Didier Stevens tools installable via pip",
-    version="2.1.2",
+    version="2.2.0",
     author="Tariq Alashaikh",
     url="https://github.com/illbison/ds-pip",
     python_requires=">=3.8",
     install_requires=requirements,
-    packages=["src", "src.lib"],
+    packages=["src"],
     entry_points={
         "console_scripts": console_scripts,
     },
--- src/lib/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-modules = [
-    module for module in os.listdir(os.path.dirname(__file__)) if module.endswith(".py") and module != "__init__.py"
-]
-
-__all__ = [module.split(".")[0] for module in modules]
